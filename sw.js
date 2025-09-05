caa16cafded99b17fecd4c49add77a03adab7b05
dan44255 <danielluciano8899@gmail.com>
2025-09-05 09:02:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-// simplified service worker code included in previous answer
+const CACHE_NAME='lifequest-v8-3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
